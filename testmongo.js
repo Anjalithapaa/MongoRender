b1507221695751993871861f280d39f95c9f5fe0
Anjali Thapa <123017097+Anjalithapaa@users.noreply.github.com>
2024-03-01 21:37:32 -0600

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://anjali:<hello123>@ckmdb.5oxvqja.mongodb.net/MangoDBProject";
+const uri = "mongodb+srv://anjali:hello123@ckmdb.5oxvqja.mongodb.net/MangoDBProject";
 
 
 // This is the standard stuff to get it to work on the browser
