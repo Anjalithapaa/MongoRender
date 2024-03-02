59852333f210e6925ced8f388ce997e77f049bce
Anjali Thapa <123017097+Anjalithapaa@users.noreply.github.com>
2024-03-01 20:47:15 -0600

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://anjalithapa:<Hello415Project>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://anjali:<hello123>@ckmdb.5oxvqja.mongodb.net/MangoDBProject";
+";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -35,8 +36,9 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+   const database = client.db('MangoDBProject');
+
+    const parts = database.collection('NewCollection');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
